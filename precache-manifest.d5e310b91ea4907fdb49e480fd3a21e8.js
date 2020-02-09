98c98aa223ab83240089da9a97b1e30b67a36682
sweida <weidaxyy@163.com>
2020-02-09 14:54:45 +0800

    Updates

---
+++ precache-manifest.d5e310b91ea4907fdb49e480fd3a21e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527838e038872eb45422e65fd465e9e7",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "1586a56c8939fd4fc219",
+    "url": "/sudoku/static/css/2.daef33c7.chunk.css"
+  },
+  {
+    "revision": "18a9803a7d01bd95f88b",
+    "url": "/sudoku/static/css/main.c66a8607.chunk.css"
+  },
+  {
+    "revision": "1586a56c8939fd4fc219",
+    "url": "/sudoku/static/js/2.ba5309c4.chunk.js"
+  },
+  {
+    "revision": "18a9803a7d01bd95f88b",
+    "url": "/sudoku/static/js/main.cdb4215e.chunk.js"
+  },
+  {
+    "revision": "305101efd92e64dc60fc",
+    "url": "/sudoku/static/js/runtime-main.6d74f11d.js"
+  }
+]);
